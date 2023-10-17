bc16fca3e4d13ed0ab74be7a1d598fce509dba96
Ilya <ipoletuev@mail.ru>
2023-10-17 19:58:48 +0300

    add table menu and items menu

---
+++ database/seeders/MenuitemsSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class MenuitemsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+
+        $items = [
+
+            ['id' => 1, 'menu_id' => 1, 'item_name' => 'Автомобили', 'parrent_item_id' => null],
+            ['id' => 2, 'menu_id' => 1, 'item_name' => 'Грузовики и спецтехника', 'parrent_item_id' => null],
+            ['id' => 3, 'menu_id' => 1, 'item_name' => 'Запчасти', 'parrent_item_id' => null],
+            ['id' => 4, 'menu_id' => 1, 'item_name' => 'Отзывы', 'parrent_item_id' => null],
+            ['id' => 5, 'menu_id' => 1, 'item_name' => 'Каталог', 'parrent_item_id' => null],
+            ['id' => 6, 'menu_id' => 1, 'item_name' => 'Шины', 'parrent_item_id' => null],
+            ['id' => 7, 'menu_id' => 1, 'item_name' => 'Еще', 'parrent_item_id' => null],
+
+            ['id' => 8, 'menu_id' => 1, 'item_name' => 'Аукционы Япониифлаг Японии', 'parrent_item_id' => 7],
+            ['id' => 9, 'menu_id' => 1, 'item_name' => 'Автомобили из Кореифлаг Кореи', 'parrent_item_id' => 7],
+        ];
+
+
+
+        DB::table('items_menu')->insert($items);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\IndexController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/items-menu', [IndexController::class, 'menu_items']);
