844fcf616ef43fb215ef781cf38ee3b1ed869603
Maksim_Alekseev <maxim040597@gmail.com>
2023-10-19 17:02:51 +0300

    добавлены items меню 'ещё'

--- database/seeders/MenuitemsSeeder.php
+++ database/seeders/MenuitemsSeeder.php
@@ -26,10 +26,48 @@ class MenuitemsSeeder extends Seeder
             ['id' => 6, 'menu_id' => 1, 'item_name' => 'Шины', 'parrent_item_id' => null],
             ['id' => 7, 'menu_id' => 1, 'item_name' => 'Еще', 'parrent_item_id' => null],
 
-            ['id' => 8, 'menu_id' => 1, 'item_name' => 'Аукционы Япониифлаг Японии', 'parrent_item_id' => 7],
-            ['id' => 9, 'menu_id' => 1, 'item_name' => 'Автомобили из Кореифлаг Кореи', 'parrent_item_id' => 7],
+            ['id' => 8, 'menu_id' => 1, 'item_name' => 'Продажа шин', 'parrent_item_id' => 7],
+            ['id' => 9, 'menu_id' => 1, 'item_name' => 'Аукционы Япониифлаг Японии', 'parrent_item_id' => 7],
+            ['id' => 10, 'menu_id' => 1, 'item_name' => 'Автомобили из Кореифлаг Кореи', 'parrent_item_id' => 7],
+            ['id' => 11, 'menu_id' => 1, 'item_name' => 'Автомобили из Германиифлаг Германии', 'parrent_item_id' => 7],
+            ['id' => 12, 'menu_id' => 1, 'item_name' => 'Электромобили', 'parrent_item_id' => 7],
+            ['id' => 13, 'menu_id' => 1, 'item_name' => 'Мотоциклы', 'parrent_item_id' => 7],
+            ['id' => 14, 'menu_id' => 1, 'item_name' => 'ОСАГО онлайн', 'parrent_item_id' => 7],
+            ['id' => 15, 'menu_id' => 1, 'item_name' => 'Автокредиты', 'parrent_item_id' => 7],
+            ['id' => 16, 'menu_id' => 1, 'item_name' => 'Проверка по VIN', 'parrent_item_id' => 7],
+            ['id' => 17, 'menu_id' => 1, 'item_name' => 'Оценить автомобиль', 'parrent_item_id' => 7],
+            ['id' => 18, 'menu_id' => 1, 'item_name' => 'Форумы', 'parrent_item_id' => 7],
+            ['id' => 19, 'menu_id' => 1, 'item_name' => 'ПДД онлайн', 'parrent_item_id' => 7],
+            ['id' => 20, 'menu_id' => 1, 'item_name' => 'Вопросы и ответы', 'parrent_item_id' => 7],
+            ['id' => 21, 'menu_id' => 1, 'item_name' => 'Рейтинг авто', 'parrent_item_id' => 7],
+            ['id' => 22, 'menu_id' => 1, 'item_name' => 'Каталог шин', 'parrent_item_id' => 7],
+            ['id' => 23, 'menu_id' => 1, 'item_name' => 'Договор купли-продажи', 'parrent_item_id' => 7],
+            ['id' => 24, 'menu_id' => 1, 'item_name' => 'Правовые вопросы', 'parrent_item_id' => 7],
+            ['id' => 25, 'menu_id' => 1, 'item_name' => 'Карта сайта', 'parrent_item_id' => 7],
+            ['id' => 26, 'menu_id' => 1, 'item_name' => 'Размещение на Дроме', 'parrent_item_id' => 7],
+            ['id' => 27, 'menu_id' => 1, 'item_name' => 'Разместить прайс', 'parrent_item_id' => 7],
+            ['id' => 28, 'menu_id' => 1, 'item_name' => 'Помощь', 'parrent_item_id' => 7],
+
         ];
 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
 
         DB::table('items_menu')->insert($items);
