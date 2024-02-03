9541cb9902d74749f22bcc593f2fab03951b37c0
Xpystum <qjq3@mail.ru>
2024-02-03 16:52:12 +0300

    add sidder user_id number

---
+++ database/migrations/2024_02_03_164311_add_column_products_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->id('user_id')->after('brand_id')->default(null);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+};
